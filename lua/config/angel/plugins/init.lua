74ec693e30eec63e5e510062a22abc295bda1802
anccode <cj_angel@outlook.com>
2023-04-15 13:08:32 -0500

    arreglando errores de actualizacion

--- lua/config/angel/plugins/init.lua
+++ lua/config/angel/plugins/init.lua
@@ -1,82 +1,30 @@
-return {
-	"nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
-	"nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
-	"kyazdani42/nvim-web-devicons",
-	"kyazdani42/nvim-tree.lua",
-	"windwp/nvim-autopairs", --nvim-autopairs
-	"numToStr/Comment.nvim", --comments
-	"akinsho/bufferline.nvim", -- for buffer
-	"moll/vim-bbye",
-	"nvim-lualine/lualine.nvim",
-	"akinsho/toggleterm.nvim",
-	"ahmedkhalf/project.nvim",
-	"lewis6991/impatient.nvim",
-	"lukas-reineke/indent-blankline.nvim",
-	"goolord/alpha-nvim",
-	"antoinemadec/FixCursorHold.nvim", -- This is needed to fix lsp doc highlight
-	"folke/which-key.nvim",
-	--tabnine
-	{ "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" },
-	{ "codota/tabnine-nvim", run = "./dl_binaries.sh" },
-	--better-comments
-	"Djancyp/better-comments.nvim",
-	--cmp
-	"hrsh7th/nvim-cmp", --plugin de autocompletado
-	"hrsh7th/cmp-buffer", --buffer autocompletado
-	"hrsh7th/cmp-path", --path autocompletado
-	"hrsh7th/cmp-cmdline", --cmd autocompletado
-	"saadparwaiz1/cmp_luasnip", --snippet autocompletado
-	"David-Kunz/cmp-npm", -- cmp-npm
-	"hrsh7th/cmp-nvim-lsp",
-	-- client databases
-	{ "kristijanhusak/vim-dadbod-ui", requires = {
-		"tpope/vim-dadbod",
-		"tpope/vim-dotenv",
-	} },
-	-- vscode-es7-javascript-react-snippets
-	{
-		"dsznajder/vscode-es7-javascript-react-snippets",
-		run = "yarn install --frozen-lockfile && yarn compile",
-	},
-	-- rest client
-	-- install in your computer curl, jq , tiny
-	{
-		"rest-nvim/rest.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-	},
-	--lsp
-	"williamboman/nvim-lsp-installer",
-	"neovim/nvim-lspconfig", -- enable LSP
-	"williamboman/mason.nvim", -- simple to use language server installer
-	"williamboman/mason-lspconfig.nvim", -- simple to use language server installer
-	"williamboman/nvim-lsp-installer", -- simple to use language server installer
-	"jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
-	"tamago324/nlsp-settings.nvim", -- language server settings defined in json for
-	--use {'neoclide/coc.nvim', branch = 'release'} --coc
-	-- telescope
-	"nvim-telescope/telescope.nvim",
-	"nvim-telescope/telescope-media-files.nvim",
-	--color scheme
-	{ "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" },
-	--snippets
-	"L3MON4D3/LuaSnip", --snippet  engine
-	"rafamadriz/friendly-snippets", --a bunch of snippet
-	--treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	},
-	"p00f/nvim-ts-rainbow",
-	"nvim-treesitter/playground",
-	"JoosepAlviste/nvim-ts-context-commentstring",
-	--git
-	"lewis6991/gitsigns.nvim",
-	{
-		"Nvchad/nvim-colorizer.lua",
-		opts = {
-			user_default_options = {
-				tailwind = true,
-			},
-		},
-	},
+-- Install your plugins here
+return {{"folke/lazy.nvim"}, -- Have packer manage itself
+{"nvim-lua/plenary.nvim"}, -- Useful lua functions used by lots of plugins
+{"windwp/nvim-autopairs"}, -- Autopairs, integrates with both cmp and treesitter
+{"numToStr/Comment.nvim"}, {"JoosepAlviste/nvim-ts-context-commentstring"}, {"kyazdani42/nvim-web-devicons"},
+        {"kyazdani42/nvim-tree.lua"}, {"akinsho/bufferline.nvim"}, {"moll/vim-bbye"}, {"nvim-lualine/lualine.nvim"},
+        {"akinsho/toggleterm.nvim"}, {"ahmedkhalf/project.nvim"}, {"lewis6991/impatient.nvim"},
+        {"lukas-reineke/indent-blankline.nvim"}, {"goolord/alpha-nvim"}, {"folke/which-key.nvim"}, -- Colorschemes
+{"folke/tokyonight.nvim"}, {"lunarvim/darkplus.nvim"}, -- Cmp 
+{"hrsh7th/nvim-cmp"}, -- The completion plugin
+{"hrsh7th/cmp-buffer"}, -- buffer completions
+{"hrsh7th/cmp-path"}, -- path completions
+{"saadparwaiz1/cmp_luasnip"}, -- snippet completions
+{"hrsh7th/cmp-nvim-lsp"}, {"hrsh7th/cmp-nvim-lua"}, -- Snippets
+{"L3MON4D3/LuaSnip"}, -- snippet engine
+{"rafamadriz/friendly-snippets"}, -- a bunch of snippets to use
+-- LSP
+{"williamboman/nvim-lsp-installer"}, {"neovim/nvim-lspconfig"}, -- enable LSP
+{"williamboman/mason.nvim"}, -- simple to use language server installer
+{"williamboman/mason-lspconfig.nvim"}, -- simple to use language server installer
+{"williamboman/nvim-lsp-installer"}, -- simple to use language server installer
+{"jose-elias-alvarez/null-ls.nvim"}, -- for formatters and linters
+{"tamago324/nlsp-settings.nvim"}, -- language server settings defined in json for
+-- for formatters and linters
+{"RRethy/vim-illuminate"}, -- Telescope
+{"nvim-telescope/telescope.nvim"}, {"nvim-telescope/telescope-media-files.nvim"}, -- Treesitter
+{"nvim-treesitter/nvim-treesitter"}, -- Git
+{"lewis6991/gitsigns.nvim"} -- Automatically set up your configuration after cloning packer.nvim
+-- Put this at the end after all plugins
 }
