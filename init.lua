bd9ececdfb7f01e35488f9c01a8772fa05642a9c
anccode <cj_angel@outlook.com>
2023-05-05 09:46:14 -0500

    solucion de error de nvim-tree

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
-require("config.plugins")
 require("config.options")
+require("config.keymaps")
+require("config.plugins")
 require("config.commands")
 require("config.lsp")
 require("config.theme")
--- lua/config/commands/init.lua
+++ lua/config/commands/init.lua
@@ -1,4 +1,3 @@
 require "config.commands.alpha"
 require "config.commands.autocommands"
-require "config.commands.keymaps"
 require "config.commands.whichkey"
