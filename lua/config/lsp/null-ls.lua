98fc017041759f783606c8a17e3ae1182d733134
anccode <cj_angel@outlook.com>
2023-05-06 13:17:26 -0500

    arreglando errores

--- lua/config/lsp/null-ls.lua
+++ lua/config/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,17 +9,17 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		--formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+  debug = false,
+  sources = {
+    --formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 
-		formatting.rome,
-		--diagnostics.eslint_d,
-		formatting.black.with({ extra_args = { "--fast" } }),
-		--format lua
-		formatting.stylua,
-        -- python 
-		--formatting.tapf,
-	  --diagnostics.flake8,
-	},
+    formatting.rome,
+    --diagnostics.eslint_d,
+    formatting.black.with({ extra_args = { "--fast" } }),
+    --format lua
+    formatting.stylua,
+    -- python
+    --formatting.tapf,
+    diagnostics.flake8,
+  },
 })
